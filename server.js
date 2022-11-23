20ffc2861515c8f588d2187b1fdb7ed072cddf37
SouzaF98 <07fabian.83@gmail.com>
2022-11-23 19:20:00 -0300

    Modulo 3 final

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/todos_os_funcionarios": "/funcionarios",
+  "/adicionar_funcionario": "/funcionarios",
+  "/editar_funcionario/:id": "/funcionarios/:id",
+  "/deletar_funcionario/:id": "/funcionarios/:id"
+
+}))
+server.use(router);
+server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
